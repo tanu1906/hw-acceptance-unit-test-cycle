604eac4b186fab6fc807eb31614ceae4dc6a9e40
EC2 Default User <ec2-user@ip-172-31-21-10.us-east-2.compute.internal>
2020-10-13 23:28:21 +0000

    HW4 Complete

--- rottenpotatoes/spec/movies_controller_spec.rb
+++ rottenpotatoes/spec/movies_controller_spec.rb
@@ -8,11 +8,13 @@ describe MoviesController, :type => :controller do
       Movie.should_receive(:find_with_same_director).with("1")
       get :similar, {:id => 1}
     end
+    
     it 'should select the Similar template for rendering when movie has a director' do
       Movie.stub(:find_with_same_director).and_return(nil,nil,false)
       get :similar, {:id => 1}
       response.should render_template('similar')
     end
+    
     it 'should make the movies with same director available to that template' do
       fake_movie = double('Movie')
       fake_results = [double('Movie'), double('Movie')]
@@ -21,17 +23,29 @@ describe MoviesController, :type => :controller do
       assigns(:movie).should == fake_movie
       assigns(:movies).should == fake_results
     end
+    
     it 'should select the Index page to redirect to when movie has no director' do
       fake_movie = double('Movie', :title => 'Aladdin')
       Movie.stub(:find_with_same_director).and_return([fake_movie,nil,true])
       get :similar, {:id => 1}
       response.should redirect_to movies_path
     end
+    
     it 'should make the error message available to that template' do
       fake_movie = double('Movie', :title => 'Aladdin')
       Movie.stub(:find_with_same_director).and_return([fake_movie, nil, true])
       get :similar, {:id => 1}
       flash[:notice].should eq("'#{fake_movie.title}' has no director info")
     end
+    
+  end
+  describe 'edit page for appropriate Movie' do
+    it 'When I go to the edit page for the Movie, it should be loaded' do
+      movie = double('Movie')
+      Movie.should_receive(:find).with('1').and_return(movie )
+      get :edit, {:id => '1'}
+      response.should be_success
+    end
+    
   end
 end
